17d46f5948b4df1216dcc2db992edb468e59834a
George <njunge995@gmail.com>
2025-10-22 01:00:48 +0300

    Add Eslint config

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
-import express from "express";
-import dotenv from "dotenv";
+import express from 'express';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello from acquisitions");
+app.get('/', (req, res) => {
+  res.status(200).send('Hello from acquisitions');
 });
 
 export default app;
